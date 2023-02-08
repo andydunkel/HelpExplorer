34cf62a2aaca4ca5d86448c8595fab4426f7118e
Andy Dunkel <andy.dunkel@ekiwi.de>
2023-02-08 20:53:49 +0100

    Basic Help Explorer, load settings etc...

--- src.net/HelpExplorer/HelpExplorer/MainWindow.xaml.cs
+++ src.net/HelpExplorer/HelpExplorer/MainWindow.xaml.cs
@@ -8,26 +8,54 @@ namespace HelpExplorer
     /// </summary>
     public partial class MainWindow
     {
+        private readonly Settings _settings = new Settings();
+        
         public MainWindow()
         {
             InitializeComponent();
             LoadIniFile();
-        }
+            
+            var obj = WebBrowser.Document as dynamic;
+                    }
 
+        /// <summary>
+        /// Load settings from ini and apply
+        /// </summary>
         private void LoadIniFile()
         {
+            _settings.LoadIni(Common.GetApplicationPath() + "\\settings.ini");
+            Title = _settings.Title;
+            WebBrowser.Navigate(Common.GetApplicationPath() + "\\" + _settings.Homepage);
+            ToolBar.Visibility = _settings.ShowToolBar ? Visibility.Visible : Visibility.Collapsed;
+            Width = _settings.WindowWidth;
+            Height = _settings.WindowHeight;
+            if (_settings.WindowCenter)
+            {
+                //Center window
+                Left = (SystemParameters.PrimaryScreenWidth - Width) / 2;
+                Top = (SystemParameters.PrimaryScreenHeight - Height) / 2;
+            }
+            else
+            {
+                Left = _settings.WindowLeft;
+                Top = _settings.WindowTop;
+            }
             
+            if (_settings.StartMaximized)
+            {
+                WindowState = WindowState.Maximized;
+            }
         }
 
         private void ToolBar_Loaded(object sender, RoutedEventArgs e)
         {
-            ToolBar toolBar = sender as ToolBar;
-            if (toolBar.Template.FindName("OverflowGrid", toolBar) is FrameworkElement overflowGrid)
+            var toolBar = sender as ToolBar;
+            if (toolBar != null && toolBar.Template.FindName("OverflowGrid", toolBar) is FrameworkElement overflowGrid)
             {
                 overflowGrid.Visibility = Visibility.Collapsed;
             }
 
-            if (toolBar.Template.FindName("MainPanelBorder", toolBar) is FrameworkElement mainPanelBorder)
+            if (toolBar != null && toolBar.Template.FindName("MainPanelBorder", toolBar) is FrameworkElement mainPanelBorder)
             {
                 mainPanelBorder.Margin = new Thickness();
             }
